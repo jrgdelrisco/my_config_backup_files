91d9e1c4c140fa2c34f81391ef0202555378df0b
jrgdelrisco <jrgdelrisco@users.noreply.github.com>
2021-11-23 08:43:06 +0100

    Adding dotnet build command using Ctrl Shift B

--- powershell7/Microsoft.PowerShell_profile.ps1
+++ powershell7/Microsoft.PowerShell_profile.ps1
@@ -18,5 +18,14 @@ Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 
+Set-PSReadLineKeyHandler -Key Ctrl+Shift+b `
+                         -BriefDescription BuildCurrentDirectory `
+                         -LongDescription "dotnet Build the current directory" `
+                         -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("dotnet build")
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+}
+
 # $GitPromptSettings.AfterPath = "`n"
 # $GitPromptSettings.DefaultPromptSuffix = " (o_O)> "
